9eed8439589952f773826b73fc088cf995126575
jh_yoon <jh_yoon@DESKTOP-7LVGASI>
2021-07-28 13:53:36 +0900

    test222

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,7 +8,7 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		
-		/*git-test-issue2 수정.*/
+		/*git-test-issue2 수정. */
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
